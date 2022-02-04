f420a1adbba6b71b53b91aac6eb4fdc7f4d8d50a
Kuzo Nazar <nazar.kuzo@outlook.com>
2022-02-04 14:38:19 +0200

    added comments

--- projects/ngx-controls/src/lib/extensions/observable.extensions.ts
+++ projects/ngx-controls/src/lib/extensions/observable.extensions.ts
@@ -2,6 +2,9 @@
 import { Observable, of } from "rxjs";
 import { catchError, map, startWith } from "rxjs/operators";
 
+/**
+ * Provides additional information about Observable request. Used by {@link trackStatus}
+ */
 export interface RequestStatus<T> {
   isLoading: boolean;
 
@@ -10,6 +13,15 @@ export interface RequestStatus<T> {
   error?: any;
 }
 
+/**
+ * Wraps Observable into a RequestStatus object that provides additional information: isLoading, error, etc.
+ * @example
+ * <ng-container *ngIf="trackStatusObservable$ | async as request">
+ *   {{ request.isLoading ? "loading" : request.value }}
+ * </ng-container>
+ * @param observable
+ * @returns
+ */
 export function trackStatus<T>(observable: Observable<T>): Observable<RequestStatus<T>> {
   return observable.pipe(
     map(value => {
