eac62df5cad4daa1ac630096170379f9ba8ef699
Kuzo Nazar <nazar.kuzo@outlook.com>
2022-02-04 14:22:17 +0200

    added services and interceptors

---
+++ projects/ngx-controls/src/lib/services/api.service.ts
@@ -0,0 +1,111 @@
+import { merge } from "lodash-es";
+import { map } from "rxjs/operators";
+import { Inject, Injectable, InjectionToken } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+
+interface DefaultHttpClientOptions {
+  headers?: HttpHeaders | {
+    [header: string]: string | string[];
+  };
+  observe: "response";
+  params?: HttpParams | {
+    [param: string]: string | string[];
+  };
+  reportProgress?: boolean;
+  responseType?: "json";
+  withCredentials?: boolean;
+}
+
+interface ApiConfig {
+  apiUrl: string;
+}
+
+export const API_CONFIG = new InjectionToken<ApiConfig>("ApiConfig");
+
+export type HttpClientOptions = Partial<DefaultHttpClientOptions>;
+
+// patch Angular HttpParams "toString()" method
+// to handle empty array query params properly
+let httpParamsToStringOriginal = HttpParams.prototype.toString;
+HttpParams.prototype.toString = function () {
+  return httpParamsToStringOriginal
+    .apply(this)
+    .replace(/&{2,}/g, "&")
+    .replace(/^&|&$/g, "");
+};
+
+@Injectable()
+export class ApiService {
+
+  private httpOptions: DefaultHttpClientOptions;
+
+  constructor(
+    private http: HttpClient,
+    @Inject(API_CONFIG) private config: ApiConfig,
+  ) {
+    this.httpOptions = {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
+      },
+      observe: "response",
+      responseType: "json"
+    };
+  }
+
+  public get<T>(url: string, params?: any, httpOptions?: HttpClientOptions) {
+    return this.http
+      .get<T>(`${this.config.apiUrl}/${url}`, this.getHttpOptions(params, httpOptions))
+      .pipe(map(response => response.body as T));
+  }
+
+  public post<T>(url: string, body?: any, params?: any, httpOptions?: HttpClientOptions) {
+    return this.http
+      .post<T>(`${this.config.apiUrl}/${url}`, JSON.stringify(body), this.getHttpOptions(params, httpOptions))
+      .pipe(map(response => response.body as T));
+  }
+
+  public put<T>(url: string, body?: any, params?: any, httpOptions?: HttpClientOptions) {
+    return this.http
+      .put<T>(`${this.config.apiUrl}/${url}`, JSON.stringify(body), this.getHttpOptions(params, httpOptions))
+      .pipe(map(response => response.body as T));
+  }
+
+  public delete<T>(url: string, params?: any, httpOptions?: HttpClientOptions) {
+    return this.http
+      .delete<T>(`${this.config.apiUrl}/${url}`, this.getHttpOptions(params, httpOptions))
+      .pipe(map(response => response.body as T));
+  }
+
+  private sanitizeQueryParams(params?: any) {
+    if (params instanceof HttpParams) {
+      params = params.keys().reduce((result: any, key: string) => {
+        result[key] = params.getAll(key);
+
+        return result;
+      }, {});
+    }
+
+    if (params instanceof Object) {
+      Object.keys(params)
+        .filter(key => params[key] == null || params[key] === "")
+        .forEach(key => {
+          delete params[key];
+        });
+    }
+
+    return params;
+  }
+
+  private getHttpOptions(queryParams?: any, httpOptions?: HttpClientOptions): DefaultHttpClientOptions {
+    if (!httpOptions) {
+      httpOptions = {} as DefaultHttpClientOptions;
+    }
+
+    httpOptions.params = this.sanitizeQueryParams(queryParams);
+
+    let result = merge({}, this.httpOptions, httpOptions);
+
+    return result;
+  }
+}
---
+++ projects/ngx-controls/src/lib/services/date-conversion.interceptor.ts
@@ -0,0 +1,24 @@
+import { tap } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from "@angular/common/http";
+import { parseDateProperty, parseDates } from "../extensions";
+
+
+@Injectable()
+export class DateConversionInterceptor implements HttpInterceptor {
+
+  public intercept(request: HttpRequest<any>, next: HttpHandler) {
+    return next
+      .handle(request)
+      .pipe(tap(event => {
+        if (event instanceof HttpResponse && event.url && event.url.indexOf("/api") >= 0) {
+          if (typeof event.body == "string") {
+            parseDateProperty(event, "body");
+          }
+          else if (typeof event.body == "object") {
+            parseDates(event.body);
+          }
+        }
+      }));
+  }
+}
