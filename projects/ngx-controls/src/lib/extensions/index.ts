1e22af29b0bfcff672ebcc56dc427b391adf5cf4
Kuzo Nazar <nazar.kuzo@outlook.com>
2022-02-07 12:32:13 +0200

    cleaned up references

--- projects/ngx-controls/src/lib/extensions/index.ts
+++ projects/ngx-controls/src/lib/extensions/index.ts
@@ -4,4 +4,3 @@ export * from "./enum.extensions";
 export * from "./observable.extensions";
 export * from "./angular.extensions";
 export * from "./router.extensions";
-export * from "./async-wrapper";
