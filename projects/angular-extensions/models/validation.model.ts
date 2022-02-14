36d7f7ff1245076492802a7afb958b1393f5bece
Kuzo Nazar <nazar.kuzo@outlook.com>
2022-02-14 13:16:16 +0200

    added angular email validation regex to use with pattern validation

--- projects/angular-extensions/models/validation.model.ts
+++ projects/angular-extensions/models/validation.model.ts
@@ -11,6 +11,11 @@ export interface AbstractControl<TValue> extends AbstractControlBase {
 
 export const CustomValidators = {
 
+  emailRegex: new RegExp(
+    `^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_\`{|}~-]+` +
+    `(?:\.[a-zA-Z0-9!#$%&'*+/=?^_\`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?` +
+    `(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`),
+
   /**
    * Triggers validation of related controls by specified predicate.
    *
