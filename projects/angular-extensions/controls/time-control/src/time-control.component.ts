0c99394db8e4361c9428c81961750f1586a8c879
Kuzo Nazar <nazar.kuzo@outlook.com>
2023-03-03 17:16:11 +0200

    improved timepicker

--- projects/angular-extensions/controls/time-control/src/time-control.component.ts
+++ projects/angular-extensions/controls/time-control/src/time-control.component.ts
@@ -1,6 +1,9 @@
-import { NgxMatDatetimePicker } from "@angular-material-components/datetime-picker";
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, ViewChild, ViewEncapsulation } from "@angular/core";
+import { MatDatepicker, MatDatepickerContent } from "@angular/material/datepicker";
+import { NgxMatTimepickerComponent } from "@angular-material-components/datetime-picker";
+
 import { ControlBase } from "angular-extensions/controls/base-control";
+import { AppMatDatepicker, AppNgxMatTimepickerComponent } from "angular-extensions/models";
 
 @Component({
   selector: "time-control",
@@ -17,9 +20,24 @@ export class TimeControlComponent<TValue> extends ControlBase<TValue> {
   @Input()
   public clearable: boolean;
 
+  @Input()
+  public disableMinute = false;
 
-  @ViewChild(NgxMatDatetimePicker, { static: true })
-  public timePicker: NgxMatDatetimePicker<any>;
+  @Input()
+  public showSeconds = false;
+
+  @Input()
+  public format = "HH:mm";
+
+  @ViewChild(MatDatepicker, { static: true })
+  public datePicker: AppMatDatepicker<Date>;
+
+  @ViewChild(NgxMatTimepickerComponent, { static: true })
+  public timePicker: AppNgxMatTimepickerComponent<Date>;
+
+  private get datepickerContent(): MatDatepickerContent<Date> | null {
+    return (this.datePicker._componentRef || this.datePicker._popupComponentRef)?.instance;
+  }
 
   constructor(
     private elementRef: ElementRef<HTMLElement>,
@@ -27,31 +45,39 @@ export class TimeControlComponent<TValue> extends ControlBase<TValue> {
   ) {
     super();
 
+    // avoid datepicker input blur while popup is open
     elementRef
       .nativeElement
-      .addEventListener("blur", event => event.stopPropagation(), { capture: true });
+      .addEventListener("blur", event => this.datepickerContent && event.stopPropagation(), { capture: true });
   }
 
   public onFieldClick(event: MouseEvent) {
     if (this.elementRef.nativeElement.querySelector(".mat-form-field-flex").contains(event.target as HTMLElement)) {
-      this.timePicker.open();
+      this.datePicker.open();
     }
 
     event.preventDefault();
   }
 
   public onToggle(event: MouseEvent) {
-    if (this.clearable && this.field.value != null) {
+    if (this.clearable && this.field.control.enabled && this.field.value != null) {
       this.field.control.setValue(null);
 
-      (this.timePicker.datepickerInput as any)._formField._control.focused = false;
+      this.field.control.markAsTouched({ onlySelf: true });
 
       event.preventDefault();
       event.stopImmediatePropagation();
 
       this.changeDetectorRef.markForCheck();
     }
+    else {
+      this.focus();
+    }
+  }
 
-    (document.activeElement as HTMLElement).blur();
+  public datePickerOpened() {
+    if (!this.timePicker._model) {
+      this.timePicker._model = new Date();
+    }
   }
 }
