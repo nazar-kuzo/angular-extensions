c3f3bf4acf3b0e5cd61100ebb404a7b0e4a2fbc9
Kuzo Nazar <nazar.kuzo@outlook.com>
2023-04-27 20:30:22 +0300

    extended start case pipe options

--- projects/angular-extensions/pipes/src/start-case.pipe.ts
+++ projects/angular-extensions/pipes/src/start-case.pipe.ts
@@ -11,14 +11,21 @@ export class StartCasePipe implements PipeTransform {
    *
    * @example "fooBar" => "Foo Bar", "--foo-bar--"" => "Foo Bar"
    * @param value Input string
-   * @param insertSpaceBeforeDigits Should inserta space before digit present in string. E.g. "every10Month" => "Every 10 Month"
+   * @param insertSpaceBeforeDigits Should insert a space before digit present in string. E.g. "every10Month" => "Every 10 Month"
+   * @param insertSpaceBeforeAbbreviations Should insert a space before abbreviation present in string. E.g. "FaceID" => "Face ID"
    * @returns Formatted string
    */
-  public transform(value?: string, insertSpaceBeforeDigits = false) {
+  public transform(value?: string, insertSpaceBeforeDigits = false, insertSpaceBeforeAbbreviations = false) {
     let label = startCase(value || "");
 
-    return insertSpaceBeforeDigits
-      ? label
-      : label.replace(/\s(\d)/g, "$1");
+    if (!insertSpaceBeforeDigits) {
+      label = label.replace(/\s(\d)/g, "$1");
+    }
+
+    if (!insertSpaceBeforeAbbreviations) {
+      label = label.replace(/\s([A-Z]{2,})/g, "$1");
+    }
+
+    return label;
   }
 }
