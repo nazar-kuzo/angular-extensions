01c31db3668931713740e2bf6050ec47569586af
Kuzo Nazar <nazar.kuzo@outlook.com>
2023-08-16 11:56:12 +0300

    updated select-all change detection

--- projects/angular-extensions/controls/select-control/src/ngx-mat-select-search.directive.ts
+++ projects/angular-extensions/controls/select-control/src/ngx-mat-select-search.directive.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, Directive, Input } from "@angular/core";
+import { Directive, Input, AfterViewInit, OnChanges } from "@angular/core";
 import { MatSelectSearchComponent } from "ngx-mat-select-search";
+
+import { SimpleChanges } from "angular-extensions/core";
 
 @Directive({
   selector: "ngx-mat-select-search"
 })
-export class MatSelectSearchComponentDirective implements AfterViewInit {
+export class MatSelectSearchComponentDirective implements AfterViewInit, OnChanges {
 
   private get checkbox() {
     return (this.selectSearchComponent.innerSelectSearch.nativeElement as HTMLElement)
@@ -17,19 +19,13 @@ export class MatSelectSearchComponentDirective implements AfterViewInit {
   }
 
   @Input()
-  public set toggleAllCheckboxDisabled(disabled: boolean) {
-    if (!this.checkbox) {
-      return;
-    }
-
-    this.checkbox.style.display = disabled ? "none" : null;
-    this.searchIcon.style.display = disabled ? null : "none";
-  }
+  public toggleAllCheckboxDisabled: boolean;
 
   constructor(
     private selectSearchComponent: MatSelectSearchComponent,
   ) {
   }
+
   public ngAfterViewInit(): void {
     if (this.checkbox) {
       this.checkbox.insertAdjacentHTML("afterend", `<mat-icon
@@ -37,7 +33,22 @@ export class MatSelectSearchComponentDirective implements AfterViewInit {
         style="padding-left: 12px; margin-right: -4px; display: none; color: #999;"
         >search</mat-icon>`);
 
-      this.toggleAllCheckboxDisabled = true;
+      this.updateCheckboxState();
     }
+  }
+
+  public ngOnChanges(changes: SimpleChanges<MatSelectSearchComponentDirective>): void {
+    if (changes.toggleAllCheckboxDisabled) {
+      this.updateCheckboxState();
+    }
+  }
+
+  private updateCheckboxState() {
+    if (!this.checkbox) {
+      return;
+    }
+
+    this.checkbox.style.display = this.toggleAllCheckboxDisabled ? "none" : null;
+    this.searchIcon.style.display = this.toggleAllCheckboxDisabled ? null : "none";
   }
 }
