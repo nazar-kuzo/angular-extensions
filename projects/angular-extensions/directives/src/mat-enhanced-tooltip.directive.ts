f0ffabbd936f1fc7d720b471fe80a1dcd5c45819
Kuzo Nazar <nazar.kuzo@outlook.com>
2023-10-09 16:03:33 +0300

    reverted tooltip enhancements

--- projects/angular-extensions/directives/src/mat-enhanced-tooltip.directive.ts
+++ projects/angular-extensions/directives/src/mat-enhanced-tooltip.directive.ts
@@ -1,54 +1,9 @@
-import { Directive, ElementRef, Input, OnInit, OnDestroy, HostListener } from "@angular/core";
-import { MatTooltip } from "@angular/material/tooltip";
+import { Directive, HostListener } from "@angular/core";
 
 @Directive({
-  selector: "[matTooltip],[matTooltipDisabled],[matTooltipEnabled]",
+  selector: "[matTooltip]",
 })
-export class MatEnhancedTooltipDirective extends MatTooltip implements OnInit, OnDestroy {
-
-  /**
-   * Tooltip message shown only when element (button, link etc.) is enabled
-   */
-  @Input()
-  public matTooltipEnabled: string;
-
-  /**
-   * Tooltip message shown only when element (button, link etc.) is disabled
-   */
-  @Input()
-  public matTooltipDisabled: string;
-
-  @Input()
-  public override set disabled(disabled: boolean) {
-    // tooltip should be always enabled
-    super.disabled = false;
-
-    this.tooltipDisabled = disabled;
-
-    if (disabled) {
-      setTimeout(() => {
-        this.elementRef.nativeElement.classList.add("disabled");
-
-        if (this.elementRef.nativeElement.attributes.getNamedItem("disabled")) {
-          this.elementRef.nativeElement.attributes.removeNamedItem("disabled");
-        }
-      });
-    }
-  }
-
-  public override get message() {
-    return this.tooltipDisabled ? this.matTooltipDisabled : this.matTooltipEnabled;
-  }
-
-  public override set message(value: string) {
-    super.message = value;
-  }
-
-  private tooltipDisabled: boolean;
-
-  private get elementRef() {
-    return (this as any)._elementRef as ElementRef<HTMLElement>;
-  }
+export class MatEnhancedTooltipDirective {
 
   /**
    * Prevents text selection when long-press on tooltip
@@ -64,14 +19,5 @@ export class MatEnhancedTooltipDirective extends MatTooltip implements OnInit, O
   @HostListener("touchend")
   public touchEnd() {
     document.body.classList.remove("no-selection");
-  }
-
-  public ngOnInit() {
-    this.elementRef.nativeElement.addEventListener("click", event => {
-      if (this.tooltipDisabled) {
-        event.stopPropagation();
-        event.preventDefault();
-      }
-    }, { capture: true });
   }
 }
